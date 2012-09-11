d24c95a40af5c14525bb6dc118e6a4c23c935ede
shylesh kumar <shylesh@gluster.com>
2012-09-11 14:52:11 +0600

    rebalance & volops
    Signed-off-by: shylesh kumar <shylesh@gluster.com>

--- rebalance.sh
+++ rebalance.sh
@@ -1,31 +1,116 @@
-#!/bin/bash
+#!/bin/bash 
 
-BRICK_DIR=/export/sda
+exit_if_error() {
+	
+ 	if [ $? -ne 0 ]; then
+		clean_and_exit	
+		
+	fi
+}
 
-gluster volume create test `hostname`:$BRICK_DIR/b1-test
-gluster volume start test
+exit_if_no_error() {
+	if [ $? -eq 0 ];then
+		echo "-ve case failed"
+		exit 1
+	fi
+}
+
+clean_and_exit() {
+	cd /
+	umount -fl $MNT_PT
+	gluster --mode=script volume stop $VOL_NAME
+	if [ $? -ne 0 ]; then
+		echo "stopping volume failed"
+		exit 1
+	fi
+	gluster --mode=script volume delete $VOL_NAME
+	if [ $? -ne 0 ]; then
+		echo "Deleteing volume failed"
+		exit 1
+	fi
+	
+}
+	 
+
+
+BRICK_DIR=/home/bricks/
+VOL_NAME=test15
+HOSTNAME=`hostname`
+MNT_PT=/mnt
+
+if [ ! -d "$BRICK_DIR" ]; then
+	echo -e "Brick directory does not exists\n"
+	exit 1
+fi
+
+gluster volume create $VOL_NAME $HOSTNAME:$BRICK_DIR/b1-$VOL_NAME
+exit_if_error
+
+gluster volume start $VOL_NAME
+exit_if_error
 sleep 4 
-mount -t glusterfs `hostname`:/test /mnt
+
+val=`mount -t glusterfs $HOSTNAME:/$VOL_NAME $MNT_PT|  grep  'already' | wc -l`
+if [ $val -ne 0 ]; then
+	echo "mount failed\n"
+	exit 1 
+fi
 sleep 5
+
 cd /mnt
 for i in {1..100} ; do dd if=/dev/urandom of=$i bs=1024 count=1000; done
-gluster volume add-brick test `hostname`:$BRICK_DIR/b2-test
-gluster volume rebalance test fix-layout start
-gluster volume rebalance test fix-layout status
-gluster volume rebalance test migrate-data start
-gluster volume rebalance test migrate-data status
-gluster volume rebalance test migrate-data start force
-gluster volume rebalance test migrate-data status
-gluster volume rebalance test migrate-data stop
+
+gluster volume add-brick $VOL_NAME $HOSTNAME:$BRICK_DIR/b2-$VOL_NAME
+exit_if_error
+
+gluster volume rebalance $VOL_NAME fix-layout start
+exit_if_error
+gluster volume rebalance $VOL_NAME status 
+exit_if_error
+gluster volume rebalance $VOL_NAME stop
+exit_if_error
+sleep 10 
+gluster volume rebalance $VOL_NAME start
+exit_if_error
+gluster volume rebalance $VOL_NAME status
+exit_if_error
+gluster volume rebalance $VOL_NAME stop
+sleep 10 
+exit_if_error
+gluster volume rebalance $VOL_NAME start force
+exit_if_error
+gluster volume rebalance $VOL_NAME status
+exit_if_error
+gluster volume rebalance $VOL_NAME stop
+exit_if_error
+
 du -h .
 dd if=/dev/urandom of=here bs=1024 count=500000
 for i in {101..200}; do mkdir $i; cd $i; touch $i;done
 
-gluster volume add-brick test `hostname`:$BRICK_DIR/b4-test `hostname`:$BRICK_DIR/b5-test
-gluster volume rebalance test fix-layout start 
-gluster volume rebalance test fix-layout status
-gluster volume rebalance test migrate-data start
+gluster volume add-brick $VOL_NAME $HOSTNAME:$BRICK_DIR/b4-$VOL_NAME $HOSTNAME:$BRICK_DIR/b5-$VOL_NAME
+exit_if_error
+gluster volume rebalance $VOL_NAME fix-layout start 
+exit_if_error
+gluster volume rebalance $VOL_NAME status
+exit_if_error
+gluster volume rebalance $VOL_NAME stop
+sleep 10
+gluster volume rebalance $VOL_NAME start
+exit_if_error
 
-gluster volume rebalance test migrate-data status
-gluster volume rebalance test migrate-data stop
+gluster volume rebalance $VOL_NAME status
+exit_if_error
+gluster volume rebalance $VOL_NAME stop
+exit_if_error
+sleep 10
+gluster volume rebalance $VOL_NAME start force
+exit_if_error
 
+gluster volume rebalance $VOL_NAME status
+exit_if_error
+gluster volume rebalance $VOL_NAME stop
+exit_if_error
+sleep 10
+echo "REBALANCE:All tests are pass"
+clean_and_exit
